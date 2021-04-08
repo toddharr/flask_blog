49e9734d722ebd5abaa4d315da71969e9874f011
Todd Harris <toddharr2@gmail.com>
2021-04-08 00:58:44 -0500

    added html, css
    html was added that includes logic for posts (dummy data was used), a nav bar, and formatting via css

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Todd',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 7, 2020'
+    },
+    {
+        'author': 'Todd Also',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 7, 2020'
+    }
+]
 
 @app.route("/")
 def home():
-    return render_template('home.html') 
+    return render_template('home.html', posts=posts) 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page!<h1>"
+    return render_template('about.html', title='About') 
 
 if __name__ == '__main__':
     app.run(debug=True)
